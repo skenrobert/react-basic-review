de041df4747ad9ef333d2b9c4bf91623dcdc16d9
kenny_robert <skenrobert@gmail.com>
2023-11-19 16:48:32 +0100

    update

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+// PascalCasing
+
+function Message() {
+
+    const name = '';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>
+    
+}
+
+export default Message;
